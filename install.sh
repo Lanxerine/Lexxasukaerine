4a63862644b075831843310f178afe6f97862027
Lanxerine <prilirorr@gmail.com>
2024-09-16 12:58:15 +0700

    Update install.sh

--- install.sh
+++ install.sh
@@ -62,7 +62,7 @@ check_token() {
   echo -e "${YELLOW}MASUKAN AKSES TOKEN :${NC}"
   read -r USER_TOKEN
 
-  if [ "$USER_TOKEN" = "jebekontol" ]; then
+  if [ "$USER_TOKEN" = "Apalah" ]; then
     echo -e "${GREEN}AKSES BERHASIL${NC}}"
   else
     echo -e "${GREEN}Buy dulu Gih Ke Lexxa${NC}"
